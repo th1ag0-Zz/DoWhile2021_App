a7b073f9d9e84880909219cdb2e3b6b1446f2831
Thiago <thiagof89.tf@gmail.com>
2021-10-20 22:24:56 -0300

    criação do cotexto de autenticação

--- web/src/components/MessageList/index.tsx
+++ web/src/components/MessageList/index.tsx
@@ -1,7 +1,56 @@
+import { useState, useEffect } from 'react';
+
+import { api } from '../../services/api';
+
+import styles from './styles.module.scss';
+import logoImg from '../../assets/logo.svg';
+
+interface Message {
+	id: string;
+	text: string;
+	user: {
+		name: string;
+		avatar_url: string;
+	};
+}
+
 export const MessageList: React.FC = () => {
+	const [messages, setMessages] = useState<Message[]>([]);
+	const [isLoading, setIsLoading] = useState(true);
+
+	useEffect(() => {
+		async function fetchMessages() {
+			const response = await api.get<Message[]>('messages/last3');
+
+			setMessages(response.data);
+			setIsLoading(false);
+		}
+
+		fetchMessages();
+	}, []);
+
 	return (
-		<div>
-			<h1>Message List</h1>
+		<div className={styles.messageListWraper}>
+			<img src={logoImg} alt='DoWhile2021' />
+
+			{isLoading ? (
+				<div />
+			) : (
+				<ul className={styles.messageList}>
+					{messages.map(message => (
+						<li key={message.id} className={styles.message}>
+							<p className={styles.messageContent}>{message.text}</p>
+
+							<div className={styles.messageUser}>
+								<div className={styles.userImage}>
+									<img src={message.user.avatar_url} alt='user' />
+								</div>
+								<span>{message.user.name}</span>
+							</div>
+						</li>
+					))}
+				</ul>
+			)}
 		</div>
 	);
 };
---
+++ web/src/contexts/auth.tsx
@@ -0,0 +1,77 @@
+import { createContext, useEffect, useState } from 'react';
+import { api } from '../services/api';
+
+interface User {
+	id: string;
+	name: string;
+	login: string;
+	avatar_url: string;
+}
+
+interface AuthContextData {
+	user: User | null;
+	signInUrl: string;
+	signOut: () => void;
+}
+
+interface AuthResponse {
+	user: User;
+	token: string;
+}
+
+export const AuthContext = createContext({} as AuthContextData);
+
+export const AuthProvider: React.FC = ({ children }) => {
+	const [user, setUser] = useState<User | null>(null);
+
+	const client_id = '0bdc8583f703b0f19b52';
+	const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=${client_id}`;
+
+	async function signIn(githubCode: string) {
+		const response = await api.post<AuthResponse>('authenticate', {
+			code: githubCode,
+		});
+
+		const { token, user } = response.data;
+
+		localStorage.setItem('@dowhile:token', token);
+
+		setUser(user);
+	}
+
+	async function signOut() {
+		setUser(null);
+		localStorage.removeItem('@dowhile:token');
+	}
+
+	useEffect(() => {
+		const token = localStorage.getItem('@dowhile:token');
+
+		if (token) {
+			api.defaults.headers.common.authorization = `Bearer ${token}`;
+
+			api.get<User>('profile').then(response => {
+				setUser(response.data);
+			});
+		}
+	}, []);
+
+	useEffect(() => {
+		const url = window.location.href;
+		const hasGithubCode = url.includes('?code=');
+
+		if (hasGithubCode) {
+			const [urlWithoutCode, githubCode] = url.split('?code=');
+
+			window.history.pushState({}, '', urlWithoutCode);
+
+			signIn(githubCode);
+		}
+	}, []);
+
+	return (
+		<AuthContext.Provider value={{ user, signInUrl, signOut }}>
+			{children}
+		</AuthContext.Provider>
+	);
+};
